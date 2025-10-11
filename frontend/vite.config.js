d51c4d3e60a8f6dd50406b22f86355e419b6dc4d
Андрей Власов <>
2025-10-11 18:38:07 +0500

    Обновление фронта

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',  // <--- добавляем, чтобы сборка использовала относительные пути
+  base: '/',  // <--- добавляем, чтобы сборка использовала относительные пути
   plugins: [react()],
   server: {
     host: true,
