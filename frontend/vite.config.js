1829df0503c74fe6ecad66d90bd2a8e9aa35be06
Андрей Власов <>
2025-10-11 18:23:34 +0500

    Обновление фронта

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: './',  // <--- добавляем, чтобы сборка использовала относительные пути
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: false
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      }
+    }
+  }
 })
